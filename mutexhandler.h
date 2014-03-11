64cfd03f02875676c1463f253999689d302cb351
Krzysztow <Krzysztow@gmail.com>
2014-03-11 18:37:57 +0000

    More comments.

--- mutexhandler.h
+++ mutexhandler.h
@@ -16,6 +16,11 @@ class MutexMsg;
 
 typedef std::map<int, MutexResourceData*> MutexesData;
 
+/**
+ * @brief The MutexHandler class that takes care of acquiring resources in a distributed system.
+ * It implements Ricart and Agrawala algorithm. It can handle many resources (identified with resourceId).
+ */
+
 class MutexHandler
 {
 public:
@@ -27,10 +32,27 @@ public:
 
     MutexHandler(MediumParticipant *medAccess, int processesId, int allProcsNo, LamportClock *clock, const std::string &dbgMsg = std::string());
 
+    /**
+     * @brief state returns actual state of the resource.
+     * @param resourceId identification of the resource.
+     * @return MutexState, depending on that if we have requested that already and gained access to it or not.
+     */
     MutexState state(int resourceId);
+    /**
+     * @brief acquire starts acquisition of mutex for a given resource.
+     */
     void acquire(int resourceId);
+    /**
+     * @brief release releases the mutex held for a given resource.
+     */
     void release(int resourceId);
 
+    /**
+     * @brief handleMessage handle mutex messages for a process.
+     * @param srcAddress process identifier of the requesting process,
+     * @param mutexMsg message body,
+     * @param msgClock Lamport clock of the message.
+     */
     void handleMessage(int srcAddress, AppMessages::MutexMsg *mutexMsg, LamportClock::LamportClockType msgClock);
 
     void setDebuggMessage(const std::string &dbgMsg);
--- networkprinter.h
+++ networkprinter.h
@@ -3,6 +3,13 @@
 
 #include "mediumparticipant.h"
 
+/**
+ * @brief The NetworkPrinter class - class simulating the network printer.
+ * It doesn't take part int the mutex acquisition process (it could, but then
+ * all the processes would have to take it into account (number of processes + 1)
+ * and it would always respond with true).
+ */
+
 class NetworkPrinter:
         public MessageReceiver
 {
